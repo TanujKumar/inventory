622708b790e4319a1aa8321612071cd9e6d366d0
tanujqlik <tanujkumar.mandale@qlik.com>
2022-03-22 08:37:19 +0530

    IT-1000 - Changed port to 5000

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');  
 var app = express();  
 app.get('/', function (req, res) {  
-   res.send('Welcome to JavaTpoint');  
+   res.send('Welcome to Test site');  
 })  
-var server = app.listen(8000, function () {  
+var server = app.listen(5000, function () {  
 var host = server.address().address;
 var port = server.address().port;
 console.log("Example app listening at http://%s:%s", host, port)  
-})  
+})
